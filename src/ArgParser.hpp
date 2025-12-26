1828e4bfdb31c4e2c45ebde669e5fdf745625b9d
Apostolos Chalis <tolishalis@gmail.com>
2025-12-26 14:28:48 +0200

    Arg parser and file IO completed

---
+++ src/ArgParser.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class ArgParser {
+public:
+    ArgParser(int argc, char** argv) {
+        // Store all arguments except the executable name (argv[0])
+        for (int i = 1; i < argc; ++i) {
+            tokens.push_back(std::string(argv[i]));
+        }
+    }
+
+    // Get argument by index (0-based)
+    std::string get(size_t index) const {
+        if (index < tokens.size()) {
+            return tokens[index];
+        }
+        return "";
+    }
+
+    // Returns how many arguments were actually passed
+    size_t count() const {
+        return tokens.size();
+    }
+
+private:
+    std::vector<std::string> tokens;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream> 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include "ArgParser.hpp"
+
+int main(int argc, char** argv) {
+	std::string algo; 
+	int starting_value;
+	int ending_value;
+	std::string filename;
+
+	ArgParser args(argc, argv); 
+
+	if (args.count() > 0) {
+		algo = args.get(0);
+		starting_value = std::stoi(args.get(1));
+		ending_value = std::stoi(args.get(2));
+		filename = args.get(3);
+
+	}
+
+	std::ofstream outFile(filename);
+
+	if (outFile.is_open()) {
+        // 3. Write data to the file (just like using std::cout)
+        outFile << "This is the content of my solution file." << std::endl;
+
+        // 4. Close the file (important to save changes!)
+        outFile.close();
+        std::cout << "File created successfully." << std::endl;
+    } else {
+        std::cerr << "Error: Could not create the file." << std::endl;
+    }
+
+	return 0;
+} 
+	
